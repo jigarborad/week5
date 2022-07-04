a85525e948eaf9cecad03ebe38a69f17aa6df0ee
Jigar Borad <73698771+jigarborad@users.noreply.github.com>
2022-07-04 12:15:46 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template
+import pandas as pd
+from flask import Flask, request, jsonify
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 model = pickle.load(open('illnesstrainer.pkl', 'rb'))
@@ -8,25 +8,24 @@ model = pickle.load(open('illnesstrainer.pkl', 'rb'))
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    if request.method == 'POST':
-        my_dict = request.form
-        try:
-            city = str(my_dict['city'])
-            gender = str(my_dict['gender'])
-            age = int(my_dict['age'])
-            income = float(my_dict['income'])
-        except ValueError:
-            return render_template('index.html')
-        else:
-            input_features = [[city, gender, age, income]]
-            prediction = model.predict(input_features)
-            if prediction[0] == 0:
-                output = 'You are not ill'
-            else:
-                output = 'You are ill'
+    if request.method == 'GET':
+        return jsonify({'data': 'hello'})
 
-            return render_template('index.html', string=output)
-    return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    city = str(request.args.get('city'))
+    gender = str(request.args.get('gender'))
+    age = int(request.args.get('age'))
+    income = float(request.args.get('income'))
+    input_features = pd.DataFrame({'City': [city], 'Gender': [gender], 'Age': [age], 'Income': [income]})
+    prediction = model.predict(input_features)
+    if prediction[0] == 0:
+        output = 'You are not ill'
+    else:
+        output = 'You are ill'
+
+    return jsonify({'Prediction': output})
 
 
 if __name__ == "__main__":
